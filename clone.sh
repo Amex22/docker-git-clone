8da01e53d3c83792fc52f7e794f35df90848fb7c
ftomasi <frederic.tomasi@gmail.com>
2016-12-15 17:06:39 +0100

    add git user and email env var

--- clone.sh
+++ clone.sh
@@ -6,6 +6,9 @@ REPO_VC_DIR=$REPO_DIR/.git
 touch /root/.ssh/known_hosts
 ssh-keyscan -T 10 `python gethost.py $REPO_URL` >> /root/.ssh/known_hosts
 
+git config --global user.email "$REPO_EMAIL"
+git config --global user.name "$REPO_NAME"
+
 if [ ! -d $REPO_VC_DIR ]
 then
     git clone $REPO_URL $REPO_DIR
